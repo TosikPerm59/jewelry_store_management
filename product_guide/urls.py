cf1eefca96a60b0be066c20508872fe898876d90
Tosikperm59 <tosikperm59@gmail.com>
2022-09-05 01:24:40 +0500

    Add login.html, autorization

--- product_guide/urls.py
+++ product_guide/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import index, product_base, register, login
+from .views import index, product_base, register, user_login, user_logout
 from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('', index, name='index'),
     path('product_base', product_base, name='product_base'),
     path('register', register, name='register'),
-    path('login', LoginView.as_view, name='login')
+    path('login', user_login, name='login'),
+    path('logout', user_logout, name='logout')
 ]
--- product_guide/views.py
+++ product_guide/views.py
@@ -1,28 +1,85 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template.context_processors import csrf
 from .models import Jewelry
+from .models import User
 from .anover_functions import search_query_processing
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
+    context = {'number_of_messages': 0}
+    user_names = User.objects.all().values('username')
+    users = User.objects.all().values()
+    username_list, message = [], []
+    for user in user_names:
+        username_list.append(user['username'])
     if request.method == 'POST':
+        condition = True
         user_name = request.POST.get('name')
         user_surname = request.POST.get('surname')
+        user_email = request.POST.get('email')
+        user_nick_name = user_name + '_' + user_surname
         user_password_1 = request.POST.get('password_1')
         user_password_2 = request.POST.get('password_2')
-        print(user_name, user_surname)
-    return render(request, 'product_guide/register.html')
+
+        context = {
+            'name': user_name,
+            'surname': user_surname,
+            'email': user_email,
+            'password_1': user_password_1,
+            'password_2': user_password_2
+        }
+
+        if user_nick_name in username_list:
+            message.append('Такой пользователь уже существует!')
+            condition = False
+        if user_password_1 != user_password_2:
+            message.append('Пароли отличаются!')
+            condition = False
+        if len(message) > 0:
+            context['messages'] = message
+            context['number_of_messages'] = len(message)
+
+        if condition:
+            user = User.objects.create_user(
+                user_nick_name,
+                user_email,
+                user_password_1
+            )
+            user.first_name = user_name
+            user.last_name = user_surname
+            user.save()
+            login(request, user)
+
+            return render(request, 'product_guide\index.html')
+
+    return render(request, 'product_guide/register.html', context=context)
 
 
-def login(request):
-    return HttpResponse('Вход')
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('login_input')
+        password = request.POST.get('password_input')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            redirect('product_guide/index.html')
+
+    return render(request, 'product_guide/login.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
 
 
 def index(request):
     return render(request, 'product_guide\index.html')
 
 
+@login_required()
 def product_base(request):
     prod_name = prod_metal = prod_uin = prod_id = prod_art = prod_weight = None
     prod_name = request.GET.get('name')
