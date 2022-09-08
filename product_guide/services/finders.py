11cb49d3e01e15b2c9d977a031ac23cdb00b8673
Tosikperm59 <tosikperm59@gmail.com>
2022-09-08 21:57:26 +0500

    Add finders.py and invoice_parser.py

---
+++ product_guide/services/finders.py
@@ -0,0 +1,353 @@
+from validity import *
+
+# Варианты возможных размеров изделий
+sizes = ['14.0', '14.5', '15.0', '15.5', '16.0', '16.5', '17.0', '17.5', '18.0', '18.5', '19.0', '19.5', '20.0',
+         '20.5', '21.0', '21.5', '22.0', '22.5', '23.0', '23.5', '24.0', '24.5', '25.0', '30.0', '35.0', '38.0',
+         '40.0', '42.0', '45.0', '50.0', '55.0', '60.0', '65.0', '70.0', '75.0']
+
+# Варианты префиксов артикула изделия относящегося к цепям или браслетам
+keywords = {'цепь': ['цб', 'нц', 'ци'], 'колье': ['шнурок'], 'браслет': ['бр', 'бк', 'нб', 'бб', 'бв', 'би']}
+
+# Варианты плетений цепей и браслетов
+keywords_weaving = {'перлина': ['шариковая', 'перлина'], 'сингапур': ['сингапур'], 'нонна': ['нонна'],
+                    'якорь': ['якорь', 'якорное'], 'бисмарк': ['бисмарк'], 'фигаро': ['фигаро', 'картье'],
+                    'снэйк': ['снэйк', 'снейк', 'кобра'], 'ромб': ['ромб'], 'love': ['love', 'лав', 'сердечки']}
+
+# Варианты имен изделий
+keywords_name = ['кольцо', 'цепь', 'серьги', 'подвеска', 'пусеты', 'браслет', 'крест', 'икона', 'колье', 'пирсинг',
+                 'моно-серьга', 'ложка']
+
+# Варианты вставок в изделия
+keywords_inserts = {'аметистом': ['аметистом', 'аметист'], 'топазом': ['топазом', 'топаз'],
+                    'аметрином': ['аметрином', 'аметрин'], 'празолитом': ['празолитом', 'празолит'],
+                    'агатом': ['агатом', 'агат'], 'гранатом': ['гранатом', 'гранат'],
+                    'фианитами': ['фианитом', 'фианит', 'фианитами'], 'турмалином': ['турмалином', 'турмалин'],
+                    'топазом Лондон': ['Лондон', 'топаз лондон', 'топазом лондон'],
+                    'ювелирным стеклом': ['стекло', 'ювелирное'], 'ониксом': ['ониксом'],
+                    'наношпинделем': ['наношпиндель'], 'кристаллом Swarovski': ['кристалл swarovski', 'swarovski'],
+                    'кристаллом премиум': ['кристалл премиум']}
+
+
+def find_barcode(_string):
+    spl_str = _string.split(' ')
+    for elem in spl_str:
+        if elem.startswith('(') and elem.endswith(')'):
+            elem = elem[1: -1]
+        if check_id(elem):
+            return elem
+
+
+def find_uin_in_giis_list(_id='', _list='', name='', metal='', weight='', art=''):
+    counter = 0
+    result = None
+    if check_id(_id):
+        for giis_dict in _list:
+            counter += 1
+            for giis_key, giis_values in giis_dict.items():
+                for item_key, item_value in giis_values.items():
+                    if 'ID' in item_key:
+                        if _id == giis_values['ID']:
+                            result = giis_key
+                            return result, counter
+                if result:
+                    break
+            if result:
+                break
+        if result is None:
+            return None, None
+
+    else:
+        similar_products = []
+        identical_products = []
+        for giis_dict in _list:
+            counter += 1
+            for giis_key, giis_values in giis_dict.items():
+                if float(giis_values['Масса'].split(' ')[0]) == weight:
+                    if (float(giis_values['Масса'].split(' ')[0]) == weight and name in giis_values['Описание']
+                            and metal in giis_values['Описание']):
+                        if 'Артикул' in giis_values:
+                            if art == giis_values['Артикул']:
+                                identical_products.append(giis_dict)
+                        else:
+                            similar_products.append(giis_dict)
+        if len(identical_products) > 0:
+            return identical_products
+        else:
+            return similar_products
+
+
+def find_uin_in_string(description):
+    split_string = description.split(' ')
+    for element in split_string:
+        if ',' in element:
+            element = element.replace(',', '')
+        if len(element) == 16 and isinteger(element):
+            return element
+
+
+def find_weight(split_string):
+    """ Функция поиска массы(вес) изделия, перебирает элементы, проверяет их на причастность к числу.
+    Возвращает вещественное число или None"""
+
+    original_string = ' '.join(split_string)
+
+    if 'вес' in split_string:
+        weight_ind = split_string.index('вес') + 1
+        split_string = [split_string[weight_ind]]
+
+    elif '—' in split_string or '---' in split_string:
+        index = split_string.index('—') if '—' in split_string else split_string.index('---')
+        split_string = split_string[index + 1:]
+
+    elif '585' in split_string or '925' in split_string:
+        index = split_string.index('585') if '585' in split_string else split_string.index('925')
+        split_string = split_string[index + 1:]
+
+    for elem in split_string:
+        elem = elem.strip()
+        try:
+            while not elem[-1].isdigit():
+                elem = elem[:-1]
+        except IndexError:
+            pass
+
+        if ',' in elem:
+            elem = elem.replace(',', '.')
+        # if elem in sizes:
+        #     print(f'Вес изделия, который определила программа, есть в списке размеров изделий. '
+        #           f'Вы вы уверены что значение {elem}, находящееся в строке ({original_string}),'
+        #           f'действительно является весом изделия?')
+        #     answer = input('введите "да", или укажите правильный вес изделия: ')
+        #     if answer == 'да':
+        #         pass
+        #     else:
+        #         elem = answer
+        if isinteger(elem):
+            elem = elem + '.00'
+        if isfloat(elem):
+            if len(elem.split('.')[1]) == 1:
+                elem = elem + '0'
+        if check_weight(elem):
+            return elem
+
+    else:
+        print(f'Программе не удалось определить вес изделия в строке {original_string}')
+        answer = ''
+        while not check_weight(answer):
+            answer = input('Укажите вес изделия в формате 0.00: ')
+        return answer
+
+
+def find_art(*args, group):
+    """ Функция анализирует столбцы Описание и Наименование, находит и проверяет артикул позиции
+      Возвращает артикул или None. """
+
+    prefixes = ['НЦ', 'ЦБ', 'ЦИ', 'ББ', 'БВ', 'БИ', 'БК', 'НБ', 'шн2/5т']
+
+    for string_from_args in args:
+
+        string_from_args = str(string_from_args).lower().split(' ')
+
+        if 'арт.' in string_from_args:
+            art_ind = string_from_args.index('арт.') + 1
+            return string_from_args[art_ind].upper()
+
+        if group == 'word':
+            if '585'in string_from_args or '925' in string_from_args:
+                ind_1 = (string_from_args.index('585') if '585' in string_from_args and 'золото' in string_from_args
+                         else string_from_args.index('925'))
+                string_from_args = string_from_args[ind_1 + 1:]
+
+                if '—' in string_from_args or '---' in string_from_args:
+                    ind_2 = (string_from_args.index('—') if '—' in string_from_args else string_from_args.index('---'))
+                    art = string_from_args[: ind_2]
+                    return ' '.join(art).upper()
+
+        for elem in string_from_args:
+            if len(elem) > 1:
+                if elem[-1] == ',':
+                    elem = elem[:-1]
+            for pref in prefixes:
+                if pref in elem:
+                    return elem.upper()
+
+            if elem.endswith('перлина') and group == 'excel':
+                return elem.replace('перлина', '').upper()
+
+            if ((elem.isdigit() and len(elem) > 3 or elem.isalnum() and not elem.isdigit()) and 2 < len(elem) != 13 and
+                    not elem.isalpha() and check_word_exceptions(elem.lower())):
+                return elem.upper()
+
+            if ('-' in elem or '_' in elem) and check_word_exceptions(elem) and len(elem) > 3:
+                return elem.upper()
+
+
+def find_name(split_string):
+    """ Метод поиска наименования изделия, сопоставляет содержимое строки со списком вариантов имен.
+    Возвращает имя или None ."""
+    for name in keywords_name:
+
+        if name in split_string:
+            return name.capitalize()
+
+    for keywords_key, keywords_values in keywords.items():
+        for value in keywords_values:
+            if value in split_string:
+                return keywords_key.capitalize()
+
+
+def find_metal(split_string):
+    """ Метод определяющий из какого металла изготовлено изделие.
+    Возвращает Название металла с пробой или None. """
+    if split_string:
+        if '585' in split_string or 'золото' in split_string:
+            return 'Золото 585'
+        elif '925' in split_string or 'серебро' in split_string:
+            return 'Серебро 925'
+    else:
+        return 'unknown'
+
+
+def find_weaving(split_string):
+    """ Метод определяющий плетения для цепей и браслетов, сопоставляет содержимое строки со списком вариантов.
+    Возвращает название плетения или None. """
+
+    for keywords_weaving_key, keywords_weaving_values in keywords_weaving.items():
+        for value in keywords_weaving_values:
+            for elem in split_string:
+                if value in elem:
+                    return keywords_weaving_key
+
+
+def find_inserts(split_string):
+    """ Метод определяющий вставки в изделия, сопоставляет содержимое строки со списком вариантов.
+    Возвращает название плетения или None. """
+
+    for keywords_inserts_key, keywords_inserts_values in keywords_inserts.items():
+        for value in keywords_inserts_values:
+            if value in split_string:
+                return keywords_inserts_key
+
+
+def find_size(split_string, group):
+    """ Метод определяющий размер изделия, перебирает елементы строки, проверяет их на причастность к числам и
+    сопоставляет с возможными вариантами размеров.
+     Возвращает вещественное число или None. """
+
+    if group == 'excel':
+        _string = ''
+        if 'размер:' in split_string:
+            size_index = split_string.index('размер:') + 1
+            _string = split_string[size_index]
+        if 'разм.' in split_string or 'разм' in split_string:
+            size_index = split_string.index('разм.') + 1 if 'разм.' in split_string else split_string.index('разм') + 1
+            _string = split_string[size_index]
+        if 'р-р' in split_string:
+            size_index = split_string.index('р-р') - 1
+            _string = split_string[size_index]
+        if 'безразмер.' in _string:
+            return 'безразм'
+        if ',' in _string:
+            _string = _string.replace(',', '.')
+        for size in sizes:
+            probable_size = _string.find(size)
+            if probable_size != -1:
+                return size
+        else:
+            for size in sizes:
+                size = str(int(float(size)))
+                probable_size = _string.find(size)
+                if probable_size != -1:
+                    return size + '.0'
+        for _string in split_string:
+            if _string.startswith('l-'):
+                return _string[2:4] + '.0'
+            if ' р-р' in _string:
+                _string = _string.replace(' р-р', '')
+                if ' ' in _string:
+                    _string = _string.replace(' ', '')
+                if ',' in _string:
+                    _string = _string.replace(',', '.')
+                if isfloat(_string) and _string in sizes:
+                    return _string
+
+    elif group == 'word':
+
+        if '—' in split_string or '---' in split_string:
+            index = (split_string.index('—') if '—' in split_string else split_string.index('---'))
+            split_string = ((' '.join(split_string).split(split_string[index])[1]).split(' '))[1:]
+
+        for el_r_part in split_string[-1::-1]:
+            if len(el_r_part) > 1:
+                if ',' in el_r_part:
+                    el_r_part.replace(',', '.')
+                if isfloat(el_r_part) and el_r_part in sizes:
+                    return el_r_part
+                if isinteger(el_r_part) and str(float(el_r_part)) in sizes:
+                    return str(float(el_r_part))
+
+
+def find_description(*args, group):
+    """ Функция анализирует столбцы Описание, Наименование и Основной металл, находит и составляет описание
+        позиции.
+        Возвращает описание позиции. """
+
+    # Инструкция для функции "find_description"
+    if group == 'word':
+
+        arg = args[0]
+        description = f'{find_name(arg)}, {find_metal(arg)}'
+
+        # Определение вставок
+        inserts = find_inserts(arg)
+
+        # Определение размера для колец, цепей и браслетов
+        size = find_size(arg, group) if (description.split(','))[0] in ('Кольцо', 'Цепь', 'Браслет', 'Колье') else None
+
+        # Определение плетения для цепей и браслетов
+        weaving = find_weaving(arg) if (description.split(','))[0] in ['Цепь', 'Браслет'] else None
+
+        if inserts:
+            description = f'{description}, c {inserts}'
+
+        if weaving:
+            description = f'{description}, плетение - {weaving}'
+
+        if size:
+            description = f'{description}, {size} р-р'
+
+        return description
+
+    if group == 'excel':
+        _name = metal = weaving = size = inserts = None
+
+        for arg in args:
+            split_arg = str(arg).lower().split(' ')
+            if not _name:
+                _name = find_name(split_arg)
+            if not metal:
+                metal = find_metal(args[-1])
+            if not weaving:
+                weaving = find_weaving(split_arg)
+            if not size:
+                size = find_size(split_arg, group)
+            if not inserts:
+                inserts = find_inserts(split_arg)
+
+        description = f'{_name}, {metal}'
+
+        if inserts:
+            description = f'{description}, c {inserts}'
+        if weaving:
+            description = f'{description}, плетение - {weaving}'
+        if size:
+            description = f'{description}, {size} р-р'
+
+        return description
+
+# Test
+
+# print(find_weight('Серьги Серебро 925 0044с-001VMз — 17,5, Кристалл Swarowsky'.split(' ')))
+
+
+# print(find_size('Цепь, Золото 585, плетение - перлина, 45.0 р-р'.split(','), group='excel'))
---
+++ product_guide/services/invoice_parser.py
@@ -0,0 +1,240 @@
+from openpyxl import Workbook
+from allFinders import *
+from servise_files import giisParser, excelReader
+from inputs import input_giis_file_path
+
+
+def invoice_parsing(path_to_excel_file):
+
+    full_rows_list, sheet, file_type, file_name, file_path = excelReader.read_excel_file(path_to_excel_file)
+    uin_ind = product_ind = weight_ind = price_ind = prod_uin = price_per_gram_ind = start = finish\
+        = row_with_provider_index = provider = row_with_date_index = invoice_number = invoice_date = col_with_number \
+        = col_with_date = code_ind = prod_barcode_from_giis = None
+    new_excel_file = Workbook()
+    new_excel_sheet = new_excel_file.active
+    products_with_size = {'кольцо': 'кольца', 'цепь': 'цепи', 'браслет': 'браслета', 'колье': 'колье', 'конго': 'конго'}
+    giis_list = giisParser.giis_file_parsing(input_giis_file_path())
+    counter = 0
+    product_list = []
+    provaiders = {'Степин': ['590500827512'],
+                  'Белышева': ['590202863882'],
+                  'Мидас': ['5904148360', '5902179700']}
+
+    if file_type == '.xlsx':
+        cols = sheet.max_column
+        rows_lst = []
+        for row in full_rows_list:
+            row_lst = []
+            for col in range(cols):
+                cell_text = sheet[row][col].value
+                if isinstance(cell_text, str):
+                    cell_text = cell_text.lower()
+                row_lst.append(cell_text)
+            rows_lst.append(row_lst)
+        full_rows_list = rows_lst
+
+    for row in full_rows_list:
+        counter += 1
+        if 'форма по окуд ' in row:
+            if 'инн' in row:
+                row_with_provider_index = counter - 1
+            else:
+                row_with_provider_index = counter - 2
+        if 'товарная накладная  ' in row or 'товарная накладная ' in row:
+            row_with_date_index = counter - 1
+        if 'страница 1' in row:
+            start = counter
+        if 'всего по накладной ' in row:
+            finish = counter - 1
+            break
+    counter = 0
+
+    row_with_provider = []
+    for elem in full_rows_list[row_with_provider_index]:
+        if elem is not None:
+            row_with_provider.append(elem)
+    for key, values in provaiders.items():
+        for value in values:
+            if value in ''.join(row_with_provider):
+                provider = key
+                break
+        if provider:
+            break
+
+    col_with_number = full_rows_list[row_with_date_index - 1].index('номер документа')
+    col_with_date = full_rows_list[row_with_date_index - 1].index('дата составления')
+    invoice_number = full_rows_list[row_with_date_index][col_with_number]
+    invoice_date = full_rows_list[row_with_date_index][col_with_date]
+    header = full_rows_list[start: start + 2]
+
+    for row in header:
+        for elem in row:
+            if elem is not None:
+                origin_elem = elem
+                elem = str(elem)
+                r_elem = ''
+                if '\n' in elem:
+                    r_elem = elem.replace('\n', '')
+
+                if r_elem.find('наименование') != -1 or elem.find('наименование') != -1:
+                    product_ind = row.index(origin_elem)
+                if r_elem.find('нетто') != -1 or elem.find('нетто') != -1:
+                    weight_ind = row.index(origin_elem)
+                if r_elem.find('сумма сучетом ндс') != -1 or elem.find('сумма сучетом ндс') != -1:
+                    price_ind = row.index(origin_elem)
+                if r_elem.find('цена') != -1 or elem.find('цена') != -1:
+                    price_per_gram_ind = row.index(origin_elem)
+                if r_elem.find('уин') != -1 or elem.find('уин') != -1:
+                    uin_ind = row.index(origin_elem)
+                if code_ind is None:
+                    if r_elem.find('код') != -1 or elem.find('код') != -1:
+                        code_ind = row.index(origin_elem)
+
+    if file_type == '.xls':
+
+        for row in full_rows_list[start + 3: finish]:
+            descr = ''
+            row_start_index, row_finish_index = None, None
+            if isinteger(row[1]) or isfloat(row[1]):
+
+                row_start_index = full_rows_list.index(row)
+                for row_2 in full_rows_list[row_start_index + 1: finish]:
+                    if (isinteger(row_2[1]) and int(row_2[1]) == int(row[1]) + 1 or isfloat(row_2[1])
+                            and int(row_2[1]) == int(row[1]) + 1 or 'Итого' in row_2):
+                        row_finish_index = full_rows_list.index(row_2)
+                        break
+
+                for row_3 in full_rows_list[row_start_index: row_finish_index]:
+                    descr += (row_3[2] + ' ' + row_3[12] + ' ').lower()
+                descr = descr.replace('\n', ' ') if '\n' in descr else descr
+                descr = descr.replace('шк:', ' ') if 'шк:' in descr else descr
+                descr = descr.replace('бирка:', ' ') if 'бирка:' in descr else descr
+                descr = descr.replace('  ', ' ') if '  ' in descr else descr
+                row[2] = descr
+                product_list.append(row)
+
+    if file_type == '.xlsx':
+
+        product_list = full_rows_list[start + 3: finish]
+
+    for product in product_list:
+        if product[1] is None or not str(product[1]).isdigit():
+            product[1] = '0'
+
+        if file_type == '.xlsx' and int(product[1]) == counter + 1 or file_type == '.xls':
+            counter += 1
+            description_string = product[product_ind].lower()
+            if '(' in description_string:
+                description_string = description_string.replace('(', '')
+            if ')' in description_string:
+                description_string = description_string.replace(')', '')
+            if ';' in description_string:
+                description_string = description_string.replace(';', '')
+            split_description_string = description_string.split(' ')
+
+            prod_name = find_name(description_string)
+            prod_metal = find_metal(description_string)
+            prod_size = find_size(split_description_string, group='excel')
+            prod_weight = product[weight_ind]
+            prod_art = find_art(description_string, group='excel')
+            prod_barcode = find_barcode(description_string)
+            prod_price = product[price_ind]
+
+            if prod_barcode is None:
+                if code_ind:
+                    prod_barcode = find_barcode(product[code_ind])
+
+            if prod_metal is None:
+                if float(prod_price) / float(prod_weight) > 1000:
+                    prod_metal = 'Золото 585'
+                else:
+                    prod_metal = 'Серебро 925'
+
+            if uin_ind:
+                prod_uin = find_uin_in_string(product[uin_ind])
+            elif prod_barcode:
+                prod_uin, string_number = find_uin_in_giis_list(_id=prod_barcode, _list=giis_list)
+            if prod_uin is None:
+                prod_uin = find_uin_in_string(description_string)
+                if prod_uin is None:
+                    prod_list = find_uin_in_giis_list(name=prod_name, metal=prod_metal, weight=prod_weight,
+                                                      art=prod_art, _list=giis_list)
+                    if prod_list:
+                        if len(prod_list) == 1:
+                            for key, value in prod_list[0].items():
+                                prod_uin = key
+                                if 'ID' in value:
+                                    prod_barcode_from_giis = value['ID']
+
+                        else:
+                            print(f'\nВыполняется поиск УИН для изделия {prod_name, prod_metal, prod_art, prod_weight}.')
+                            print(f'Найдено {len(prod_list)} строк с похожим описанием изделия')
+                            count = 0
+                            for prod in prod_list:
+                                count += 1
+                                print(f'Строка {count}: {prod}')
+                            print('\nВыберите наиболее подходящую строку и введите ее номер.')
+                            number = input('Введите номер строки или ENTER, чтобы пропустить выбор: ')
+                            if number.isdigit():
+                                for key, value in prod_list[int(number) - 1].items():
+                                    prod_uin = key
+                                    if 'ID' in value:
+                                        prod_barcode_from_giis = value['ID']
+
+                        if prod_barcode and prod_barcode_from_giis:
+                            if prod_barcode_from_giis != prod_barcode:
+                                prod_barcode = prod_barcode_from_giis
+
+            if not prod_metal:
+                if product[price_per_gram_ind] > 2500:
+                    prod_metal = 'Золото 585'
+                else:
+                    prod_metal = 'Серебро 925'
+
+            prod_description = f'{prod_name}, {prod_metal} ('
+
+            if prod_art:
+                prod_description = f'{prod_description}арт. {prod_art}'
+
+            if prod_uin:
+                prod_description = f'{prod_description}, уин {prod_uin}'
+            prod_description = f'{prod_description}, вес {float(prod_weight)} г.'
+
+            if prod_name is None:
+                print(f'Программе не удалось определить имя изделия в строчке {description_string}.')
+                prod_name = input('Введите название изделия: ')
+
+            if prod_name.lower() in products_with_size.keys():
+                if not prod_size:
+                    print(f'\nПрограмме не удалось определить размер для {products_with_size[prod_name.lower()]}'
+                          f' в строке {counter}.')
+                    print(f'Укажите размер для изделия: {prod_description}')
+                    prod_size = input('Введите размер в формате 00.0: ')
+            if prod_size:
+                prod_description = f'{prod_description}, р-р {prod_size}'
+            prod_description = f'{prod_description})'
+            print(prod_description, prod_barcode)
+
+            """Принты для теста функции"""
+            # print(prod_description, prod_barcode, prod_price, provider)
+
+            new_excel_sheet['A' + str(counter)] = prod_description
+            new_excel_sheet['B' + str(counter)] = str(prod_barcode)
+            new_excel_sheet['C' + str(counter)] = prod_art
+            new_excel_sheet['D' + str(counter)] = 'Товар'
+            new_excel_sheet['E' + str(counter)] = prod_price
+            new_excel_sheet['F' + str(counter)] = provider
+            new_excel_sheet['G' + str(counter)] = 'шт'
+            new_excel_sheet['H' + str(counter)] = '1'
+            new_excel_sheet['I' + str(counter)] = f'{invoice_date}'
+            new_excel_sheet['J' + str(counter)] = f'Накладная {invoice_number}, {prod_metal}'
+
+        prod_name = prod_metal = prod_inserts = prod_weaving = prod_art = prod_uin = prod_barcode \
+            = prod_barcode_from_giis = None
+
+    path_for_save = f'{file_path}\\Номенклатура для({file_name}).xlsx'
+    new_excel_file.save(path_for_save)
+
+# Test
+# invoice_parsing(
+#     'E:\Elena\Documents\Документы  Александрова Е.П\Накладные\Входящие накладные\Степин Евгений\\05.09.22\965.xls')
