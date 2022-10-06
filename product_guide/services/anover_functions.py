f73c881233d054ef2e966136a9ad565f06a0d7ca
Tosikperm59 <tosikperm59@gmail.com>
2022-10-07 00:08:58 +0500

    added "make_product_dict_from_paginator" function and "form_type_check" function, changed "get_context_for_product_list" function and "calculate_weight_number_price" function

--- product_guide/services/anover_functions.py
+++ product_guide/services/anover_functions.py
@@ -1,6 +1,12 @@
+from django.core.paginator import Paginator
+
+
 from product_guide.models import File
 from django.core.exceptions import ObjectDoesNotExist
 import xlrd
+
+from product_guide.services.readers import read_excel_file
+from product_guide.services.validity import isfloat
 
 
 def search_query_processing(search_string):
@@ -15,52 +21,37 @@ def search_query_processing(search_string):
     return prod_name, prod_metal, prod_uin, prod_id, prod_art, prod_weight
 
 
-def make_product_dict_from_dbqueryset(dbqueryset):
-
-    """ Функция создания словаря, для отображения в представлениях.
-        Принимает QuerySet из базы данных и возвращает словарь словарей изделий с органиченным набором ключей."""
-
-    product_dict_for_view, product_dicts_dict = {}, {}
-    counter = 0
-    for product_dict_from_dbqueryset in dbqueryset.values():
-        counter += 1
-        product_dict_for_view[counter] = {'name': product_dict_from_dbqueryset['name'],
-                                          'metal': product_dict_from_dbqueryset['metal'],
-                                          'barcode': product_dict_from_dbqueryset['barcode'],
-                                          'uin': product_dict_from_dbqueryset['uin'],
-                                          'weight': product_dict_from_dbqueryset['weight'],
-                                          'vendor_code': product_dict_from_dbqueryset['vendor_code'],
-                                          'size': product_dict_from_dbqueryset['size'],
-                                          'price': product_dict_from_dbqueryset['price'],
-                                          'number': counter
-                                          }
-        product_dicts_dict[counter] = product_dict_from_dbqueryset
-
-    return product_dicts_dict
-
-
-def calculate_weight_number_price(product_list):
+def calculate_weight_number_price(products_dicts_dict):
+    # print(products_dicts_dict)
     counter = 0
     total_weight = 0
     total_price = 0
-    for product in product_list:
+    for key, product in products_dicts_dict.items():
+        # print(product['weight'], type(product['weight']))
+        # print(product)
         counter += 1
-        if type(product['weight']) is float:
-            total_weight += product['weight']
+        try:
+            if isfloat(product['weight']):
+
+                total_weight += float(product['weight'])
+        except:
+            pass
     return total_weight, counter
 
 
-def get_context_for_product_list(product_list):
+def get_context_for_product_list(products_dicts_dict, page_num):
     counter = 0
-    total_weight, number_of_products = calculate_weight_number_price(product_list)
-
-    for product in product_list:
-        counter += 1
-        product['number'] = counter
-
+    total_weight, number_of_products = calculate_weight_number_price(products_dicts_dict)
+    product_queryset = make_product_queryset_from_dict_dicts(products_dicts_dict)
+    # print('NUM =', page_num)
+    # print(products_dicts_dict)
+    product_list = []
+    paginator = Paginator(product_queryset, 50)
+    page = paginator.get_page(page_num)
     context = {
-        'product_list': product_list,
+        'product_list': page.object_list,
         'list_length': len(product_list),
+        'num_pages': [x for x in range(paginator.num_pages + 1)][1:],
         'total_weight': round(total_weight, ndigits=2),
         'len_products': number_of_products
     }
@@ -69,7 +60,6 @@ def get_context_for_product_list(product_list):
 
 
 def save_invoice(form, file_name):
-
     try:
         file = File.objects.get(title=file_name)
         file.delete()
@@ -82,9 +72,60 @@ def save_invoice(form, file_name):
     file_object.save()
 
 
-# def form_type_check(invoice_path):
-#     document = docx.Document(invoice_path)
-#     print(document.paragraphs[0].text)
-#     if 'Унифицированная форма № ТОРГ-12' in document.paragraphs[0].text:
-#         if len(document.paragraphs) == 3:
-#             return 'Торг12'
+def form_type_check(rows_list, sheet, file_name):
+    if file_name == '4_BATCH_LIST_PRINT.xlsx':
+        return 'giis_report'
+
+
+def make_product_dict_from_paginator(paginator):
+    product_dict = {}
+    count = paginator.count
+    for c in range(count):
+        product_list = paginator.get_page(c + 1)
+        for product in product_list.object_list:
+            for key, value in product.items():
+                product_dict[key] = value
+
+    return product_dict
+
+
+def make_product_queryset_from_dict_dicts(dict_dicts):
+    product_queryset = []
+    for key, product in dict_dicts.items():
+        product_queryset.append(product)
+
+    return product_queryset
+
+
+def make_product_dict_from_dbqueryset(dbqueryset):
+    """ Функция создания словаря, для отображения в представлениях.
+        Принимает QuerySet из базы данных и возвращает словарь словарей изделий с органиченным набором ключей."""
+
+    product_dict_for_view, product_dicts_dict = {}, {}
+    counter = 0
+    for product_dict_from_dbqueryset in dbqueryset:
+        counter += 1
+        product_dict_from_dbqueryset['number'] = counter
+
+        # product_dict_for_view[counter] = product_dict_from_dbqueryset.values()
+        # product_dict_for_view[counter] = {'name': product_dict_from_dbqueryset['name'],
+        #                                   'metal': product_dict_from_dbqueryset['metal'],
+        #                                   'barcode': product_dict_from_dbqueryset['barcode'],
+        #                                   'uin': product_dict_from_dbqueryset['uin'],
+        #                                   'weight': product_dict_from_dbqueryset['weight'],
+        #                                   'vendor_code': product_dict_from_dbqueryset['vendor_code'],
+        #                                   'size': product_dict_from_dbqueryset['size'],
+        #                                   'price': product_dict_from_dbqueryset['price'],
+        #                                   'number': counter
+        #                                   }
+        product_dicts_dict[counter] = product_dict_from_dbqueryset
+    return product_dicts_dict
+
+
+def filters_check(product_name, product_metal):
+    if product_name == 'all' and product_metal == 'all':
+        return 'all'
+    else:
+        return 'filtered'
+
+
